85ab36993f0ce06a79ad04441b25c2645edcd7a5
Wolf <turk.brandon@gmail.com>
2026-01-12 15:12:40 -0800

    Fix handleError

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -9,7 +9,7 @@ module.exports = {
 		waiting: 'controls.handleMediaEvent',
 		ratechange: 'controls.handleMediaEvent',
 		timeupdate: 'controls.updateDuration',
-		loadedmetadata: [ 'controls.updateDuration', 'controls' ],
+		loadedmetadata: [ 'controls.updateDuration', 'controls.preventWrapping' ],
 		durationchange: 'controls.updateDuration',
 		volumechange: 'controls.updateVolume',
 		loadstart: 'controls.pollForLoading',
